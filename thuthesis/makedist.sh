0d32a57c231ebd972b6e807907939f5d99f7c9ab
Edward L. Fox <edyfox@gmail.com>
2006-03-24 04:13:53 +0000

    Minor change.

--- thuthesis/makedist.sh
+++ thuthesis/makedist.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: $
+# $Id: makedist.sh 47 2006-03-20 03:47:47Z edyfox $
 
 dirs="data figures ref"
 list_example="data/* figures/* main.pdf main.tex ref/* shuji.pdf shuji.tex"
@@ -15,24 +15,24 @@ daysold=`daysold`
 
 for dir in $dirs
 do
-    mkdir -p example-$daysold/$dir
-    mkdir -p template-$daysold/$dir
+    mkdir -p example-$daysold/thuthesis/$dir
+    mkdir -p template-$daysold/thuthesis/$dir
 done
 
 for file in $list_example
 do
-    cp -f $file example-$daysold/$file
+    cp -f $file example-$daysold/thuthesis/$file
 done
 
 for file in $list_template
 do
-    cp -f $file template-$daysold/$file
+    cp -f $file template-$daysold/thuthesis/$file
 done
 
 pushd example-$daysold
-tar jcvf ../example-$daysold.tar.bz2 *
+tar jcvf ../example-$daysold.tar.bz2 ./
 popd
 
 pushd template-$daysold
-tar jcvf ../template-$daysold.tar.bz2 *
+tar jcvf ../template-$daysold.tar.bz2 ./
 popd
