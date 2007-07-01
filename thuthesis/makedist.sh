3594be3ce57dc2c26894f07681b4ce1438aa26e6
Edward L. Fox <edyfox@gmail.com>
2007-07-01 23:54:07 +0000

    Deleted the unnecessary extra files from the distributed tarball.

--- thuthesis/makedist.sh
+++ thuthesis/makedist.sh
@@ -4,8 +4,7 @@
 dirs="data figures ref"
 list_example="data/*.tex figures/* ref/*.bib main.tex shuji.tex main.pdf  shuji.pdf"
 list_template="thuthesis.ins thuthesis.dtx thubib.bst Readme thuthesis.pdf Makefile msmake.cmd thutils.sty"
-list_extra="thuthesis.cls thuthesis.cfg"
-list_all="$list_example $list_template $list_extra"
+list_all="$list_example $list_template"
 
 if [ $0 != ./makedist.sh ]; then
     echo "This script can only be run in the same directory of the script."
