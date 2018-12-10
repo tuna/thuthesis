4a612bcfdd666cb485f805fc3faab72e4b4dba3a
Zeping Lee <zepinglee@gmail.com>
2018-12-10 21:14:27 +0800

    remove eps files

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,6 @@ const config = {
         files: ['thuthesis.ins',
                 'thuthesis.dtx',
                 'tsinghua.pdf',
-                'tsinghua.eps',
                 'thuthesis-numeric.bst',
                 'thuthesis-author-year.bst',
                 'Makefile',
