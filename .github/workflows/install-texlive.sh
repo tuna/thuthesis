f40015cf2b0aa45a54254adbff659fd6a79aa2bc
Harry Chen <i@harrychen.xyz>
2020-06-02 01:46:17 +0800

    Add more dependencies of markdown pacakge
    Signed-off-by: Harry Chen <i@harrychen.xyz>

--- .github/workflows/install-texlive.sh
+++ .github/workflows/install-texlive.sh
@@ -19,7 +19,8 @@ REQUIRED_PKGS="$XETEX_PKGS $CTEX_PKGS bibunits caption enumitem etoolbox \
     filehook footmisc notoccite pdfpages titlesec threeparttable unicode-math";
 FONT_PKGS="fandol tex-gyre xits";
 EXTRA_PKGS="booktabs $HYPERREF_PKGS $NOMENCL_PKGS ntheorem siunitx";
-DOC_PKGS="hologo listings xcolor markdown";
+MARKDOWN_PKGS="markdown csvsimple gobble"
+DOC_PKGS="hologo listings xcolor $MARKDOWN_PKGS";
 EXAMPLE_PKGS="diagbox float fp metalogo multirow pict2e"
 
 tlmgr install $BIN_PKGS $REQUIRED_PKGS $FONT_PKGS $EXTRA_PKGS $DOC_PKGS \
