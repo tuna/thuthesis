00d042aae48c437c5e88bfb2316931a0be273b55
Zeping Lee <zepinglee@gmail.com>
2020-06-09 17:12:27 +0800

    Set packtdszip = true

--- build.lua
+++ build.lua
@@ -33,6 +33,12 @@ unpackexe = "xetex"
 checkopts = "-file-line-error -halt-on-error -interaction=nonstopmode"
 typesetopts = "-shell-escape -file-line-error -halt-on-error -interaction=nonstopmode"
 
+packtdszip = true
+
+tdslocations = {
+  "bibtex/bst/thuthesis/*.bst",
+}
+
 lvtext = ".tex"
 
 function docinit_hook()
