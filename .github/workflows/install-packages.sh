9ddd0fad3a2972d803772001c2ceb7999adf063f
Zeping Lee <zepinglee@gmail.com>
2020-09-07 16:33:15 +0800

    Update test files

--- .github/workflows/install-packages.sh
+++ .github/workflows/install-packages.sh
@@ -6,14 +6,14 @@ export PATH="/opt/texlive/bin/x86_64-linux:$PATH";
 XETEX_PKGS="fontname fontspec l3packages xetex";
 CTEX_PKGS="cjk ctex environ ms trimspaces ulem xecjk zhnumber";
 HYPERREF_PKGS="bitset letltxmacro pdfescape pdflscape";
-BIBLATEX_PKGS="biber biblatex biblatex-gb7714-2015 xstring";
+BIBLATEX_PKGS="biber biblatex biblatex-apa biblatex-gb7714-2015 biblatex-mla xstring";
 NOMENCL_PKGS="nomencl koma-script xkeyval";
 
 BIN_PKGS="latexmk l3build";
 REQUIRED_PKGS="$XETEX_PKGS $CTEX_PKGS bibunits caption enumitem etoolbox \
     filehook footmisc notoccite pdfpages soul titlesec threeparttable unicode-math";
 FONT_PKGS="fandol tex-gyre xits";
-EXTRA_PKGS="booktabs $HYPERREF_PKGS $BIBLATEX_PKGS $NOMENCL_PKGS ntheorem siunitx";
+EXTRA_PKGS="apacite booktabs $HYPERREF_PKGS $BIBLATEX_PKGS $NOMENCL_PKGS ntheorem siunitx";
 MARKDOWN_PKGS="markdown fancyvrb csvsimple gobble"
 DOC_PKGS="hologo listings xcolor $MARKDOWN_PKGS";
 EXAMPLE_PKGS="float fp metalogo multirow"
