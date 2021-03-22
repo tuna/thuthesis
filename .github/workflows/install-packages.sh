6e842dd5c7b0f2041970e47b8726e32be294ac3e
Zeping Lee <zepinglee@gmail.com>
2021-03-22 21:42:02 +0800

    Add algorithms test files

--- .github/workflows/install-packages.sh
+++ .github/workflows/install-packages.sh
@@ -13,7 +13,7 @@ BIN_PKGS="latexmk l3build";
 REQUIRED_PKGS="$XETEX_PKGS $CTEX_PKGS bibunits caption enumitem etoolbox \
     filehook footmisc notoccite pdfpages soul titlesec threeparttable unicode-math";
 FONT_PKGS="fandol tex-gyre xits";
-EXTRA_PKGS="apacite booktabs $HYPERREF_PKGS $BIBLATEX_PKGS $NOMENCL_PKGS ntheorem siunitx";
+EXTRA_PKGS="algorithms apacite booktabs $HYPERREF_PKGS $BIBLATEX_PKGS $NOMENCL_PKGS ntheorem siunitx";
 MARKDOWN_PKGS="markdown fancyvrb csvsimple gobble"
 DOC_PKGS="hologo listings xcolor $MARKDOWN_PKGS";
 EXAMPLE_PKGS="float fp metalogo multirow mwe"
